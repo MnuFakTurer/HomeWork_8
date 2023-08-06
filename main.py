90e8c394d85c6b9988e0cb015835d053cf877a60
Олександр <Pioner322622@gmail.com>
2023-08-06 17:51:53 +0300

    task 1

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+
+# task 1
+
+# try:
+#
+#     phone_number = input("Enter phone number of Ukraine: ")
+#
+#     if re.match(r'^(?:(?:\+380|380|)\d{9}|098\d{7})$', phone_number):
+#         print("Phone number is valid.")
+#     else:
+#         print("Phone number is invalid!")
+#
+# except Exception as e:
+#     print(e)
