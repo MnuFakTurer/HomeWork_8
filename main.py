73a209b4ed08a88347ade6cdf3d17606bab99598
Олександр <Pioner322622@gmail.com>
2023-08-06 17:52:43 +0300

    task 2

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ import re
 #
 # except Exception as e:
 #     print(e)
+
+# task 2
+
+# try:
+#
+#     home_number = input("Enter phone number of Dnipro: ")
+#
+#     if re.match(r'^(?:(?:380|)\d{9}|056\d{7})$', home_number):
+#         print("Phone number is valid.")
+#     else:
+#         print("Phone number is invalid!")
+#
+# except Exception as e:
+#     print(e)
